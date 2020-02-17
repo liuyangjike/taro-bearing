cc986337a04f8d84081032e20770aa551e3d7d45
jikeliuyang <liuyang02@hujiang.com>
2020-02-17 11:46:45 +0800

    feat(*):mode 3

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -84,275 +84,197 @@ export default class Index extends Component {
 
   drawBearing = () => {
 
-    function drawRoundedRect(ctx, x, y, width, height, d, r, ball, fill, stroke) {
-      ctx.beginPath() // draw top and top right corner 
+  function drawRoundedRect2(ctx, x, y, width, height, d, r, ball, fill, stroke) {
+    const bearing = [
+      {
+        x: x,  //0
+        y: y + r  //0
+      },
+      {
+        x: x,  //1
+        y: y   //1
+      },
+      {
+        x: x + r,  //2
+        y: y   //2
+      },
+      {
+        x: x + width,  //3
+        y: y   //3
+      },
+      {
+        x: x + width,  //4
+        y: y + r   //4
+      },
+      {
+        x: x + width,  //5
+        y: y + (height - d) / 4 - ball * Math.cos(Math.PI / 4)   //5
+      },
+      {
+        x: x + width/2 + ball* Math.sin(Math.PI / 4),  //6
+        y: y + (height - d) / 4 - ball * Math.cos(Math.PI / 4)   //6
+      },
+      {
+        x: x + width/2,   //7
+        y: y + (height - d) / 4 //7
+      },
+      {
+        x: x ,   //8
+        y: y + (height - d) / 4 - ball * Math.cos(Math.PI / 4)    //8
+      },
+      {
+        x: x ,   //9
+        y: y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)    //9
+      },
+      {
+        x: x + width/2 - ball* Math.sin(Math.PI / 4),   //10
+        y: y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)    //10
+      },
+      {
+        x: x + width,   //11
+        y: y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)    //11
+      },
+      {
+        x: x + width,   //12
+        y: y + (height - d) / 2    //12
+      },
+      {
+        x: x ,   //13
+        y: y + (height - d) / 2    //13
+      },
+      {
+        x: x ,   //14
+        y: y + height - (height - d) / 2    //14
+      },
+      {
+        x: x + width ,   //15
+        y: y + height - (height - d) / 2    //15
+      },
+      {
+        x: x + width ,   //16
+        y: y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4) //16
+      },
+      {
+        x: x + width/2 + ball* Math.sin(Math.PI / 4),    //17
+        y: y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4) //17
+      },
+      {
+        x: x + width/2,    //18
+        y: y + height - (height - d) / 4//18
+      },
+      {
+        x: x ,    //19
+        y: y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4) //18
+      },
+      {
+        x: x ,    //20
+        y: y + height - (height - d) / 4 + ball * Math.cos(Math.PI / 4) //20
+      },
+      {
+        x: x + width/2 - ball* Math.sin(Math.PI / 4),     //21
+        y: y + height - (height - d) / 4 + ball * Math.cos(Math.PI / 4) //21
+      },
+      {
+        x: x + width,     //22
+        y: y + height - (height - d) / 4 + ball * Math.cos(Math.PI / 4) //22
+      },
+      {
+        x: x + width,     //23
+        y: y + height//23
+      },
+      {
+        x: x + width - r ,     //24
+        y: y + height  //24
+      },
+      {
+        x: x,     //25
+        y: y + height  //25
+      },
+      {
+        x: x,     //26
+        y: y + height - r  //26
+      },
+    ]
 
-      // outer
-      ctx.moveTo(x + r, y)
-      ctx.arcTo(x + width, y, x + width, y + r, r); // draw right side and bottom right corner 
-      ctx.arcTo(x + width, y + height, x + width - r, y + height, r); // draw bottom and bottom left corner 
-      ctx.arcTo(x, y + height, x, y + height - r, r); // draw left and top left corner 
-      ctx.arcTo(x, y, x + r, y, r)
-
-      ctx.moveTo(x , y + (height - d) / 2)
-      ctx.lineTo(x + width , y + (height - d) / 2)
-      ctx.moveTo(x , y + (height - d) / 2 + d)
-      ctx.lineTo(x + width , y + (height - d) / 2 + d)
-
-      // ball
-      const x2 = x + width/ 2
-      const y2 =  y + (height - d) / 4
-      ctx.moveTo( x2 + ball , y2)
-      ctx.arc(x2, y2, ball, 0, 2 * Math.PI)
-      const x3 = x + width/ 2
-      const y3 =  y + height - (height - d) / 4
-      ctx.moveTo( x3 + ball , y3)
-      ctx.arc(x3, y3, ball, 0, 2 * Math.PI)
-
-      // line
-      const x4 = x
-      const y4 =  y + (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-      ctx.moveTo( x4 , y4)
-      ctx.lineTo( x4 + width/2 - ball* Math.sin(Math.PI / 4) , y4)
-      ctx.moveTo( x4 + width/2 + ball* Math.sin(Math.PI / 4), y4)
-      ctx.lineTo( x4 + width, y4)
-
-      const x5 = x
-      const y5 =  y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-      ctx.moveTo( x5 , y5)
-      ctx.lineTo( x5 + width/2 - ball* Math.sin(Math.PI / 4) , y5)
-      ctx.moveTo( x5 + width/2 + ball* Math.sin(Math.PI / 4), y5)
-      ctx.lineTo( x5 + width, y5)
-
-
-      const x6 = x
-      const y6 =  y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-      ctx.moveTo( x6 , y6)
-      ctx.lineTo( x6 + width/2 - ball* Math.sin(Math.PI / 4) , y6)
-      ctx.moveTo( x6 + width/2 + ball* Math.sin(Math.PI / 4), y6)
-      ctx.lineTo( x6 + width, y6)
-
-      const x7 = x
-      const y7 =  y + height - (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-      ctx.moveTo( x7 , y7)
-      ctx.lineTo( x7 + width/2 - ball* Math.sin(Math.PI / 4) , y7)
-      ctx.moveTo( x7 + width/2 + ball* Math.sin(Math.PI / 4), y7)
-      ctx.lineTo( x7 + width, y7)
-
-      if (fill) { ctx.fill() }
-      if (stroke) { ctx.stroke() }
-      ctx.draw()
-  }
-
-
-  function drawRoundedRect1(ctx, x, y, width, height, d, r, ball, fill, stroke) {
-    ctx.beginPath() // draw top and top right corner 
-    // outer
-
-    // 1. outer 上半
-    ctx.moveTo(x + r, y)
-    ctx.arcTo(x + width, y, x + width, y + r, r); // draw right side and bottom right corner 
-    const x1 = x + width
-    const y1 = y + (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x1 , y1)
-
-    const x2 = x + width/2 + ball* Math.sin(Math.PI / 4)
-    const y2 = y + (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x2 , y2)
-
-    const x3 = x + width/ 2
-    const y3 =  y + (height - d) / 4
-    ctx.arc(x3, y3, ball, -Math.PI / 4, - Math.PI * 3 / 4, true)
-
-    const x4 = x
-    const y4 = y + (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x4 , y4)
-
-    const x5 = x
-    const y5 = y + r
-    ctx.lineTo(x5 , y5)
-
-    ctx.arcTo(x , y, x + r, y, r); // draw bottom and bottom left corner 
-    context.setStrokeStyle("#111")
-
-    ctx.fillStyle = "#66bbdd"
+    // outer 上
+    ctx.moveTo(bearing[0].x, bearing[0].y)
+    ctx.arcTo(bearing[1].x, bearing[1].y, bearing[2].x, bearing[2].y, r); // draw right side and bottom right corner 
+    ctx.arcTo(bearing[3].x, bearing[3].y, bearing[4].x, bearing[4].y, r); // draw right side and bottom right corner 
+    ctx.lineTo(bearing[5].x, bearing[5].y)
+    ctx.lineTo(bearing[6].x, bearing[6].y)
+    ctx.arc(bearing[7].x, bearing[7].y, ball, - Math.PI / 4, - Math.PI * 3 / 4, true)
+    ctx.lineTo(bearing[8].x, bearing[8].y)
+    ctx.lineTo(bearing[0].x, bearing[0].y)
     ctx.fill()
 
-     // 2. inner 上半
-    const x6 = x + width
-    const y6 = y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    ctx.moveTo(x6, y6)
-
-    const x7 = x + width/2 + ball* Math.sin(Math.PI / 4)
-    const y7 = y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x7 , y7)
-
-    const x8 = x + width/ 2
-    const y8 =  y + (height - d) / 4
-    ctx.arc(x8, y8, ball, - Math.PI *  7/ 4, - Math.PI * 5 / 4, false)
-
-    const x9 = x
-    const y9 = y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x9 , y9)
-
-    const x10 = x
-    const y10 = y + (height - d) / 2 
-    ctx.lineTo(x10 , y10)
-
-    const x11 = x + width
-    const y11 = y + (height - d) / 2
-    ctx.lineTo(x11 , y11)
-
-    const x12 = x + width
-    const y12 = y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x12 , y12)
+    // inner 上
+    ctx.moveTo(bearing[9].x, bearing[9].y)
+    ctx.lineTo(bearing[10].x, bearing[10].y)
+    ctx.arc(bearing[7].x, bearing[7].y, ball, Math.PI * 3 / 4, Math.PI / 4, true)
+    ctx.lineTo(bearing[11].x, bearing[11].y)
+    ctx.lineTo(bearing[12].x, bearing[12].y)
+    ctx.lineTo(bearing[13].x, bearing[13].y)
+    ctx.lineTo(bearing[9].x, bearing[9].y)
     ctx.fill()
 
-
-    // 4. inner 下半
-    const x14 = x + width
-    const y14 = y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    ctx.moveTo(x14, y14)
-
-    const x15 = x + width/2 + ball* Math.sin(Math.PI / 4)
-    const y15 = y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x15 , y15)
-
-    const x16 = x + width/ 2
-    const y16 =  y + height - (height - d) / 4
-    ctx.arc(x16, y16, ball, - Math.PI / 4, - Math.PI * 3 / 4, true)
-
-    const x17 = x 
-    const y17 = y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x17 , y17)
-
-    const x18 = x 
-    const y18 = y + height - (height - d) / 2
-    ctx.lineTo(x18 , y18)
-
-    const x19 = x + width
-    const y19 = y + height - (height - d) / 2
-    ctx.lineTo(x19 , y19)
-
-    const x20 = x + width
-    const y20 = y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x20 , y20)
+    // inner 下
+    ctx.moveTo(bearing[14].x, bearing[14].y)
+    ctx.lineTo(bearing[15].x, bearing[15].y)
+    ctx.lineTo(bearing[16].x, bearing[16].y)
+    ctx.lineTo(bearing[17].x, bearing[17].y)
+    ctx.arc(bearing[18].x, bearing[18].y, ball, - Math.PI  / 4, - Math.PI * 3 / 4, true)
+    ctx.lineTo(bearing[19].x, bearing[19].y)
+    ctx.lineTo(bearing[14].x, bearing[14].y)
     ctx.fill()
 
-    // 5. outer 下半
-    const x21 = x + width
-    const y21 = y + height - (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    ctx.moveTo(x21, y21)
-
-    const x22 = x + width/2 + ball* Math.sin(Math.PI / 4)
-    const y22 = y + height - (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x22 , y22)
-
-    const x23 = x + width/ 2
-    const y23 =  y + height - (height - d) / 4
-    ctx.arc(x23, y23, ball, Math.PI / 4, Math.PI * 3 / 4, false)
-
-    const x24 = x 
-    const y24 = y + height - (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    ctx.lineTo(x24 , y24)
-
-    ctx.arcTo(x , y + height, x + r, y + height, r); // draw right side and bottom right corner 
-    ctx.arcTo(x + width , y + height, x + width, y + height - r, r); // draw right side and bottom right corner 
-    ctx.lineTo(x21 , y21)
+    // outer 下
+    ctx.moveTo(bearing[20].x, bearing[20].y)
+    ctx.lineTo(bearing[21].x, bearing[21].y)
+    ctx.arc(bearing[18].x, bearing[18].y, ball, Math.PI * 3 / 4,  Math.PI / 4, true)
+    ctx.lineTo(bearing[22].x, bearing[22].y)
+    ctx.arcTo(bearing[23].x, bearing[23].y, bearing[24].x, bearing[24].y, r); // draw right side and bottom right corner 
+    ctx.arcTo(bearing[25].x, bearing[25].y, bearing[26].x, bearing[26].y, r); // draw right side and bottom right corner 
+    ctx.lineTo(bearing[20].x, bearing[20].y)
     ctx.fill()
 
-    // // 3. 闭合线
-    ctx.moveTo(x1, y1)
-    ctx.lineTo(x6 , y6)
-
-    ctx.moveTo(x4, y4)
-    ctx.lineTo(x9 , y9)
-
-    ctx.moveTo(x14, y14)
-    ctx.lineTo(x21 , y21)
-
-    ctx.moveTo(x17, y17)
-    ctx.lineTo(x24 , y24)
-
-    ctx.moveTo(x11, y11)
-    ctx.lineTo(x19 , y19)
-
-    ctx.moveTo(x10, y10)
-    ctx.lineTo(x18 , y18)
-
-    ctx.moveTo(x2, y2)
-    ctx.arc(x3, y3, ball, - Math.PI / 4, Math.PI  / 4, false)
-
-    const x13 = x + width/2 - ball* Math.sin(Math.PI / 4)
-    const y13 = y + (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-
-    ctx.moveTo(x13, y13)
-    ctx.arc(x3, y3, ball, - Math.PI * 3 / 4, Math.PI * 3 / 4, true)
-
-
-    ctx.moveTo(x15, y15)
-    ctx.arc(x16, y16, ball, - Math.PI / 4, Math.PI  / 4, false)
-
-    const x25 = x + width/2 - ball* Math.sin(Math.PI / 4)
-    const y25 = y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    ctx.moveTo(x25, y25)
-    ctx.arc(x16, y16, ball, - Math.PI * 3 / 4, Math.PI * 3 / 4, true)
-
-
-
-
-    // const x19 = x + width
-    // const y19 = y + height - (height - d) / 2
-    // ctx.lineTo(x19 , y19)
-
-    // const x20 = x + width
-    // const y20 = y + height - (height - d) / 4 - ball * Math.cos(Math.PI / 4)
-    // ctx.lineTo(x20 , y20)
-    // ctx.fill()
-
-
-
- 
-    //  const x9 = x
-    //  const y9 = y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    //  ctx.lineTo(x9 , y9)
- 
-    //  const x10 = x
-    //  const y10 = y + (height - d) / 2 
-    //  ctx.lineTo(x10 , y10)
- 
-    //  const x11 = x + width
-    //  const y11 = y + (height - d) / 2
-    //  ctx.lineTo(x11 , y11)
- 
-    //  const x12 = x + width
-    //  const y12 = y + (height - d) / 4 + ball * Math.cos(Math.PI / 4)
-    //  ctx.lineTo(x12 , y12)
-    //  ctx.fill()
-
-
-
-
-
-
+    //闭合
+    ctx.moveTo(bearing[8].x, bearing[8].y)
+    ctx.lineTo(bearing[9].x, bearing[9].y)
+    ctx.moveTo(bearing[5].x, bearing[5].y)
+    ctx.lineTo(bearing[11].x, bearing[11].y)
+    ctx.moveTo(bearing[19].x, bearing[19].y)
+    ctx.lineTo(bearing[20].x, bearing[20].y)
+    ctx.moveTo(bearing[16].x, bearing[16].y)
+    ctx.lineTo(bearing[22].x, bearing[22].y)
+    ctx.moveTo(bearing[13].x, bearing[13].y)
+    ctx.lineTo(bearing[14].x, bearing[14].y)
+    ctx.moveTo(bearing[12].x, bearing[12].y)
+    ctx.lineTo(bearing[15].x, bearing[15].y)
+    ctx.moveTo(bearing[10].x, bearing[10].y)
+    ctx.arc(bearing[7].x, bearing[7].y, ball,  Math.PI * 3 / 4, - Math.PI * 3 / 4, false)
+    ctx.moveTo(bearing[6].x, bearing[6].y)
+    ctx.arc(bearing[7].x, bearing[7].y, ball,  - Math.PI / 4, Math.PI  / 4, false)
+    ctx.moveTo(bearing[21].x, bearing[21].y)
+    ctx.arc(bearing[18].x, bearing[18].y, ball, Math.PI * 3 / 4, - Math.PI * 3 / 4, false)
+    ctx.moveTo(bearing[17].x, bearing[17].y)
+    ctx.arc(bearing[18].x, bearing[18].y, ball,  - Math.PI / 4, Math.PI  / 4, false)
 
 
     ctx.stroke()
-
-
-
-
     ctx.draw()
+  }
 
-}
+
 
     // 使用 wx.createContext 获取绘图上下文 context
     var context = Taro.createCanvasContext('bearing')
 
-    context.setStrokeStyle("#111")
     context.setLineWidth(1)
+    context.setStrokeStyle("#111")
+
+    context.fillStyle = "#66bbdd"
     // context.rect(0, 0, 70, 220)
-    drawRoundedRect1(context, 100, 200, 70, 220, 80, 5, 20, false, true)
+    drawRoundedRect2(context, 100, 200, 70, 220, 80, 5, 20, false, true)
     // context.stroke()
     // context.draw()
     // context.fill()
